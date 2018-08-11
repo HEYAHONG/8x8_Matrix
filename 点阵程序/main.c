3956bbd0bce55513f040e11cb793c9ddc1b13727
HEYAHONG <H17729826337@163.com>
2018-08-11 16:48:47 +0800

    修改主程序，为将stc15w104修改为8x8矩阵主控做准备，下一步通过剩余的io口模拟一个与其它单片机通信的接口，本机作为从机。

--- 点阵程序/main.c
+++ 点阵程序/main.c
@@ -13,7 +13,9 @@ unsigned char  code Display_word[2][8] = {
 0x0C,0x1E,0x3E,0x7C,0x7C,0x3E,0x1E,0x0C //桃心（形状） 
 };
 
-//SPI通信函数 发送一个直接数据给点阵模块
+unsigned char VBuff[8]; //LED显示缓冲区
+
+//发送一个直接数据给点阵模块
 void Send_Byte( unsigned char dat)
 {
 	unsigned char i;//循环次数变量
@@ -42,22 +44,74 @@ void Send_Byte( unsigned char dat)
 	}		
 }
 
+void CopyCodeToBuff(unsigned char code  *src) //从程序区复制指定数据到显示缓冲区
+{
+unsigned char k=0;
+for(k = 0 ; k < 8 ; k++)		
+{
+VBuff[k]=*src++;
+}
+}
+
+void CopyToBuff(unsigned char code  *src) //从数据区区复制指定数据到显示缓冲区
+{
+unsigned char k=0;
+for(k = 0 ; k < 8 ; k++)		
+{
+VBuff[k]=*src++;
+}
+}
+
+/*以下是从stc15示例代码复制并加以修改的部分，需用到STC15的一些特性，可能不能在某些兼容51的单片机上使用*/
+#define FOSC 11059200L	 //系统时钟11.0592M
+
+//#define T1MS (65536-FOSC/1000)      //1T模式
+#define T50MS (65536-FOSC/12/20) //12T模式
+unsigned  long SysClk=0;  //系统时钟记录变量
+void tm0_isr() interrupt 1 	//定时器0中断处理函数
+{
+    SysClk++;
+	{ //其它需要在定时器0处理的事
+		if(SysClk%40==0) 
+		{
+		CopyCodeToBuff(Display_word[0]);
+		}
+		if(SysClk%40==20)
+		 {
+		CopyCodeToBuff(Display_word[1]);
+		}
+		if(SysClk== 4000000000)	 SysClk=0; //系统时钟上界
+	}
+	
+}
+void init_tm0()	//定时器0初始化
+{
+  // AUXR |= 0x80;                   //定时器0为1T模式
+   AUXR &= 0x7f;                   //定时器0为12T模式
+
+    TMOD = 0x00;                    //设置定时器为模式0(16位自动重装载)
+    TL0 = T50MS;                     //初始化计时值
+    TH0 = T50MS >> 8;
+    TR0 = 1;                        //定时器0开始计时
+    ET0 = 1;                        //使能定时器0中断
+    EA = 1;
+}
+
+/*stc15修改部分结束*/
+
 void main()
 {
 
 	unsigned char k, temp;
-	unsigned int i;//i的数值越大显示停留的时间就越久
+	init_tm0(); //初始化定时器0
 
-	while(1)
+	while(1)   //将缓冲区的内容放到LED点阵上。
 	{
-			//亲
-		for(i = 0 ; i < 1000 ; i++)
-		{
 			temp = 0x7f;
 			for(k = 0 ; k < 8 ; k++)		//一个字母8BYTE
 			{
 				Send_Byte(temp);			//列选择
-				Send_Byte(Display_word[0][k]);			//行数据
+				Send_Byte(VBuff[k]);			//行数据
 				LEDARRAY_LAT = 1;					//锁存数据
 				_nop_();
 			
@@ -65,23 +119,7 @@ void main()
 				_nop_();
 				temp = (temp>>1)|0x80;
 			}
-		}
-			//桃心（形状） 
-		for(i = 0 ; i < 1000 ; i++)
-		{
-			temp = 0x7f;
-			for(k = 0 ; k < 8 ; k++)		//一个字母4BYTE
-			{
-				Send_Byte(temp);			//列选择
-				Send_Byte(Display_word[1][k]);			//行数据
-				LEDARRAY_LAT = 1;					//锁存数据
-				_nop_();
+		
 			
-				LEDARRAY_LAT = 0;
-				_nop_();
-				temp = (temp>>1)|0x80;
-			}
-		}
-		P10=~P10; //LED	
 	}
 }
